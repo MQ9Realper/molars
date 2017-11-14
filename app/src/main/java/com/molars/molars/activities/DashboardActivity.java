68796263151106706d84d387c5dc995dfae98cb7
xts400 <dennis@threadstack.co>
2017-11-14 15:00:37 +0300

    Add dashboard

---
+++ app/src/main/java/com/molars/molars/activities/DashboardActivity.java
@@ -0,0 +1,15 @@
+package com.molars.molars.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.molars.molars.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
--- app/src/main/java/com/molars/molars/activities/MainActivity.java
+++ app/src/main/java/com/molars/molars/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.molars.molars.activities;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
@@ -25,7 +27,19 @@ public class MainActivity extends AppCompatActivity {
         initAnimation();
     }
 
-    private void initAnimation(){
+    private void initAnimation() {
         YoYo.with(Techniques.FadeIn).duration(2800).repeat(0).playOn(layoutCustomLogo);
+
+        goToDashboard();
+    }
+
+    private void goToDashboard() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, DashboardActivity.class);
+                startActivity(i);
+            }
+        }, 2800);
     }
 }
